5ba52197326168ae960e983d728ca2a09962673c
sendrastisch <sandra.fernandes@student.hu.nl>
2022-09-20 21:46:37 +0200

    Add use cases to service (not done yet and commented bc i dont have database yet)

--- src/main/java/nl/hu/bep2/casino/blackjack/application/BlackjackService.java
+++ src/main/java/nl/hu/bep2/casino/blackjack/application/BlackjackService.java
@@ -29,27 +29,54 @@ public class BlackjackService {
         return game.getGameDTO();
     }
 
-//    public GameDTO hit(){
+    //uitgecomment want dit werkt nog niet. heb nog geen database.
+
+//    public GameDTO hit(String playerName){
+//
 //        Game game = gameRepository.findById(gameId).orElseThrow(() -> new NoGamesFoundException("Wrong id."));
-//        game.hit();
+//        game.playerHit();
+//        //sla spel op
+//        //geef chips indien gewonnen
+//        chipsService.depositChips(playerName, game.calculatePayout());
 //
-//    }
-
-//    public GameDTO stand(){
-//
-//    }
-//
-//    public GameDTO surrender(){
-//
+//        return game.getGameDTO();
 //    }
 //
-//    public GameDTO doubleDown(){
+//    public GameDTO stand(String playerName){
+//        Game game = gameRepository.findById(gameId).orElseThrow(() -> new NoGamesFoundException("Wrong id."));
+//        game.stand();
 //
+//        //sla spel op
+//
+//        //geef chips indien gewonnen
+//        chipsService.depositChips(playerName, game.calculatePayout());
+//
+//        return game.getGameDTO();
 //    }
-
-
-
-
+//
+//    public GameDTO surrender(String playerName){
+//        Game game = gameRepository.findById(gameId).orElseThrow(() -> new NoGamesFoundException("Wrong id."));
+//        game.surrender();
+//
+//        //sla spel op
+//
+//        //geef chips indien gewonnen
+//        chipsService.depositChips(playerName, game.calculatePayout());
+//
+//        return game.getGameDTO();
+//    }
+//
+//    public GameDTO doubleDown(String playerName){
+//        Game game = gameRepository.findById(gameId).orElseThrow(() -> new NoGamesFoundException("Wrong id."));
+//        game.doubleDown();
+//
+//        //sla spel op
+//
+//        //geef chips indien gewonnen
+//        chipsService.depositChips(playerName, game.calculatePayout());
+//
+//        return game.getGameDTO();
+//    }
 
 
 }
