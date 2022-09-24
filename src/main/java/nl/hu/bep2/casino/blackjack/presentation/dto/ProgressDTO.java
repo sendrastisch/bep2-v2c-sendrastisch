8cb6d00dd63e49a1f96b67a660b6f392ef5b25d1
sendrastisch <sandra.fernandes@student.hu.nl>
2022-09-24 14:25:31 +0200

    change name dto

--- src/main/java/nl/hu/bep2/casino/blackjack/presentation/dto/ProgressDTO.java
+++ src/main/java/nl/hu/bep2/casino/blackjack/presentation/dto/ProgressDTO.java
@@ -1,22 +1,23 @@
-package nl.hu.bep2.casino.blackjack.application.dto;
+package nl.hu.bep2.casino.blackjack.presentation.dto;
 
 import nl.hu.bep2.casino.blackjack.domain.Card;
 import nl.hu.bep2.casino.blackjack.domain.enums.GameState;
 
 import java.util.ArrayList;
 
-public class GameDTO {
+public class ProgressDTO {
     public String username;
     public long bet;
     public ArrayList<Card> dealerHand;
     public ArrayList<Card> playerHand;
     public GameState state;
 
-    public GameDTO(String username, long bet, ArrayList<Card> dealerHand, ArrayList<Card> playerHand, GameState state) {
+    public ProgressDTO(String username, long bet, ArrayList<Card> dealerHand, ArrayList<Card> playerHand, GameState state) {
         this.username = username;
         this.bet = bet;
         this.dealerHand = dealerHand;
         this.playerHand = playerHand;
         this.state = state;
     }
+
 }
