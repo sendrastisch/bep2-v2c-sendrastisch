4315dcb4f374824020136b093c0376092a34cdf4
sendrastisch <sandra.fernandes@student.hu.nl>
2022-10-07 18:40:37 +0200

    voeg id toe aan progressdto

--- src/main/java/nl/hu/bep2/casino/blackjack/presentation/dto/ProgressDTO.java
+++ src/main/java/nl/hu/bep2/casino/blackjack/presentation/dto/ProgressDTO.java
@@ -7,13 +7,16 @@ import nl.hu.bep2.casino.blackjack.domain.enums.GameState;
 import java.util.ArrayList;
 
 public class ProgressDTO {
+    public long id;
+
     public String username;
     public long bet;
     public HandDTO dealerHand;
     public HandDTO playerHand;
     public GameState state;
 
-    public ProgressDTO(String username, long bet, HandDTO dealerHand, HandDTO playerHand, GameState state) {
+    public ProgressDTO(long id, String username, long bet, HandDTO dealerHand, HandDTO playerHand, GameState state) {
+        this.id = id;
         this.username = username;
         this.bet = bet;
         this.dealerHand = dealerHand;
